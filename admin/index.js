47f54c3e71ad25272ca4cf102ea64f9913f7787b
pitikdmitry <pitik.dmitry@mail.ru>
2019-04-21 19:59:37 +0300

    changed port

--- admin/index.js
+++ admin/index.js
@@ -32,7 +32,7 @@
   };
 
   loadCategories = function () {
-    fetch('http://localhost:8080/api/category/get_all', {
+    fetch('http://localhost:80/api/category/get_all', {
             mode: 'cors',
 
 
@@ -69,7 +69,7 @@
     };
 
     formData.set('main_img', fileList[0]);
-    request.open("POST", 'http://localhost:8080/api/post/add');
+    request.open("POST", 'http://localhost:80/api/post/add');
     request.send(formData);
   };
 })();
