4c7eb8554da39c4e502d79b5acd0e48807f4b205
pitikdmitry <pitik.dmitry@mail.ru>
2019-04-21 14:14:24 +0300

    set category title unique

--- src/database/models.py
+++ src/database/models.py
@@ -7,7 +7,7 @@ PostCategories = Table(
     'post_categories',
     metadata,
     Column('id', Integer, primary_key=True),
-    Column('title', String(length=100), nullable=False),
+    Column('title', String(length=100), nullable=False, unique=True),
 )
 
 Post = Table(
