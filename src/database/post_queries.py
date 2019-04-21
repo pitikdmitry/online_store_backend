bbf9b50ba94ac09bc0f94e6d51527a6a7779848c
pitikdmitry <pitik.dmitry@mail.ru>
2019-04-21 14:12:53 +0300

    adding post to db

---
+++ src/database/post_queries.py
@@ -0,0 +1,13 @@
+from typing import Dict
+
+from database.models import Post
+
+
+async def add_post(conn, data: Dict[str, str]):
+    stmt = Post.insert().values(category_id=data['category_id'],
+                                title=data['title'],
+                                text=data['text'],
+                                main_img=data['main_img'],
+                                created_at=data['created_at'],
+                                last_updated=data['last_updated'])
+    await conn.execute(stmt)
