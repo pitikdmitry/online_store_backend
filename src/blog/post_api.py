c85d345d9db2fda0126311db6ccb54dbd6b78aaf
pitikdmitry <pitik.dmitry@mail.ru>
2019-04-21 14:12:35 +0300

    adding post to db

--- src/blog/post_api.py
+++ src/blog/post_api.py
@@ -1,38 +1,52 @@
-# import ujson
+import logging
+from datetime import datetime
+
 from aiohttp import web, MultipartReader, hdrs
 
+from database.category_queries import get_category_by_title
 from database.models import PostCategories
+from database.post_queries import add_post
 from utils.const import ROOT_DIR
+
+
+logger = logging.getLogger('post_api')
 
 
 async def add(request: web.Request) -> web.Response:
     async with request.app['db'].acquire() as conn:
         reader = MultipartReader.from_response(request)
-        metadata = None
-        filedata = None
+        data = {}
+
         while True:
             part = await reader.next()
             if part is None:
                 break
 
-            print(f"part_namne: {part.name}")
+            logger.debug(f"part_name: {part.name}")
 
             if hdrs.CONTENT_TYPE in part.headers and (part.headers[hdrs.CONTENT_TYPE] == 'image/jpeg' or \
                 part.headers[hdrs.CONTENT_TYPE] == 'image/png'):
                 metadata = await part.read()
-                print(metadata)
                 path = ROOT_DIR + '/static/' + part.name + '.jpg'
-                with open(ROOT_DIR + '/static/' + part.name + '.jpg', "wb") as f:
+                with open(path, "wb") as f:
                     f.write(metadata)
-                continue
+
+                data['main_img'] = path
             else:
                 metadata = await part.text()
-                name = part.name
-                print(metadata)
 
+                if part.name == 'category':
+                    category_id = await get_category_by_title(conn, title=metadata)
+                    data['category_id'] = category_id
+                    logger.info(f'category_id: {category_id}')
+                else:
+                    data[part.name] = metadata
 
-            print(part)
-        # body = await request.read()
+        current_time = datetime.now()
+        data['created_at'] = current_time
+        data['last_updated'] = current_time
+
+        await add_post(conn, data)
 
     return web.json_response("OK")
 
