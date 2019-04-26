23ae664cbb2ee45fc8736b178b111c3286567a96
pitikdmitry <pitik.dmitry@mail.ru>
2019-04-26 13:49:43 +0300

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get --assume-yes install docker.io && sudo apt-get --assume-yes install docker-compose && sudo systemctl start docker && sudo systemctl enable docker && sudo apt-get --assume-yes install python3-pip && sudo apt-get --assume-yes install git 
+sudo apt-get update && sudo apt-get --assume-yes install docker.io && sudo apt-get --assume-yes install docker-compose && sudo systemctl start docker && sudo systemctl enable docker && sudo service docker start && sudo apt-get --assume-yes install python3-pip && sudo apt-get --assume-yes install git 
